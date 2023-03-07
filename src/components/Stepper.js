bdf05a68d219caf603143acbca92d9f166a44078
gdemartino93 <113302882+gdemartino93@users.noreply.github.com>
2023-03-07 12:36:27 +0100

    style all steps

--- src/components/Stepper.js
+++ src/components/Stepper.js
@@ -1,5 +1,5 @@
 import React, {useRef} from 'react'
-
+import Form from 'react-bootstrap/Form'
 const Stepper = () => {
     // useref vanno dichiarati nulli poichè al caricamento della pagina questi elementi non esistono
     const cognome = useRef(null);
@@ -14,36 +14,65 @@ const Stepper = () => {
     }
   return (
     <section>
-        <div className="step d-flex justify-content-center align-items-center">
-            <form>
-            <label>Nome</label>
-            <input type="text"></input>
-            <a className="btn btn-success" onClick={()=>handleRef(cognome)}>GO</a>
-            </form>
-        </div>
-        <div className="step d-flex justify-content-center align-items-center">
-            <form>
-            <label>cognome</label>
-            <input ref={cognome} type="text"></input>
-            <a className="btn btn-success" onClick={()=>handleRef(dateOB)}>GO</a>
-            </form>
-        </div>
-        <div className="step d-flex justify-content-center align-items-center">
-            <form>
-            <label>eta</label>
-            <input type="text" ref={dateOB}></input>
-            <a className="btn btn-success" onClick={()=>handleRef(city)}>GO</a>
-
-            </form>
-        </div>
-        <div className="step d-flex justify-content-center align-items-center">
-            <form>
-            <label>Citta</label>
-            <input type="text" ref={city}></input>
-            <button className="btn btn-success" type='submit' onClick={()=>handleRef(city)}>Registrati</button>
-
-            </form>
-        </div>
+        
+            <div className="step d-flex justify-content-center align-items-center">    
+                <div className='d-flex flex-column row'>
+                    <div>
+                        <label>Nome</label>  
+                    </div>
+                    <div className='d-flex justify-content-center align-items-center g-3'>
+                        <Form.Control  type="text" placeholder='Inserisci Nome'></Form.Control>
+                        <a className="btn btn-success mx-3" onClick={()=>handleRef(cognome)}>GO</a>
+                    </div>
+                </div> 
+            </div>      
+        
+        <form>
+            <div className="step d-flex justify-content-center align-items-center">    
+                <div className='d-flex flex-column row'>
+                    <div>
+                        <label>Nome</label>  
+                    </div>
+                    <div className='d-flex justify-content-center align-items-center g-3'>
+                        <Form.Control  type="text" placeholder='Inserisci Nome'></Form.Control>
+                        <a className="btn btn-success mx-3" onClick={()=>handleRef(cognome)}>GO</a>
+                    </div>
+                </div> 
+            </div>
+            <div className="step d-flex justify-content-center align-items-center">    
+                <div className='d-flex flex-column row'>
+                    <div>
+                        <label>Cognome</label>  
+                    </div>
+                    <div className='d-flex justify-content-center align-items-center g-3'>
+                        <Form.Control  type="text" placeholder='Inserisci Cognome' ref={cognome}></Form.Control>
+                        <a className="btn btn-success mx-3" onClick={()=>handleRef(dateOB)}>GO</a>
+                    </div>
+                </div> 
+            </div>
+            <div className="step d-flex justify-content-center align-items-center">    
+                <div className='d-flex flex-column row'>
+                    <div>
+                        <label>Data di nascita</label>  
+                    </div>
+                    <div className='d-flex justify-content-center align-items-center g-3'>
+                        <Form.Control  type="date" ref={dateOB}></Form.Control>
+                        <a className="btn btn-success mx-3" onClick={()=>handleRef(city)}>GO</a>
+                    </div>
+                </div> 
+            </div>
+            <div className="step d-flex justify-content-center align-items-center">    
+                <div className='d-flex flex-column row'>
+                    <div>
+                        <label>Città</label>  
+                    </div>
+                    <div className='d-flex justify-content-center align-items-center g-3'>
+                        <Form.Control  type="email" placeholder='Inserisci città' ref={city}></Form.Control>
+                        <button className="btn btn-success mx-3" type='submit'>GO</button>
+                    </div>
+                </div> 
+            </div>
+        </form>
     </section>
   )
 }
